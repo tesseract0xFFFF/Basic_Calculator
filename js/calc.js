299d65062e9f43f1e9dc0eaf5ff20867c8288c0c
tesseractx0FFFF <itkugit321@gmail.com>
2023-03-19 19:24:39 +0200

    added a function 'operate' that takes two numbers and an operator and then calls the relevant operation function.

--- js/calc.js
+++ js/calc.js
@@ -15,5 +15,35 @@ function multiply(a, b){
     return a * b;
 }
 
+function operate(operator, num1, num2){
+
+    let result = 0;
+
+    // calls the right operation function depending on the operator provided.
+    switch(operator){
+
+        case '+':
+            result = add(num1, num2);
+            break;
+
+        case '-':
+            result = subtract(num1, num2);
+            break;
+
+        case '/':
+            result = divide(num1, num2);
+            break;
+
+        case '*':
+            result = multiply(num1, num2);
+            break;
+            
+    }
+
+    return result;
+}
+ 
+let calc = operate ('-', 10, 2);
+
 
 
