66652400f35a931eef7444872ecd2f80c5be3398
tesseractx0FFFF <itkugit321@gmail.com>
2023-03-22 20:56:53 +0200

    added event listeners to the operator buttons. added a mechanism that checks whether two numbers were provided. can now chain calculations one after another.

--- js/calc.js
+++ js/calc.js
@@ -43,9 +43,50 @@ function operate(operator, num1, num2){
     return result;
 }
 
+function areThereTwoArguments(e){
+// stores the operator.
+    operatorCount++;
+    // function will always be called exactly once for a pair of numbers. 
+    // this way, the array index won't be incremented without evaluation.
+    if(operatorCount > 1){
+        return;
+    }
+    
+// checks whether a two numbers were provided.
+    if(arrIndex === 0){
+        currentOperator = e.target.textContent; //stores the operator relevant for current 2 number evaluation.
+        calcDisplay.textContent = currentOperator;
+// checks if there is a first number.
+        if(numbers[0] === ''){
+            return;
+        }
+        arrIndex++;
+        numbers[arrIndex] = '';
+
+        
+    }
+    else {
+        // when 2 numbers are provided - carry the operation on them,
+        //  store result in the 1st array element and zero the index. then print result onto screen.
+        numbers[0] = operate(currentOperator, +numbers[0], +numbers[1]);
+        numbers[1] = 0;
+        arrIndex = 0;
+        operatorCount = 0;
+        currentOperator = '';
+        calcDisplay.textContent = numbers[0];
+    }
+} 
+
+
 let calcBody = document.querySelector('.calcBody');
 let buttonArea = document.querySelector('.buttonArea');
 let calcDisplay = document.querySelector('.calcDisplay');
+
+// initialize an empty array to store numbers in and an array index.
+let numbers = [''];
+let arrIndex = 0;
+let currentOperator = '';
+let operatorCount = 0;
 
 
 
@@ -58,12 +99,35 @@ for (i = 0; i <= 9; i++){
    calcButton.setAttribute('class', 'button');
    calcButton.setAttribute('id', `${i}`);
    calcButton.textContent = `${i}`;
-   calcButton.addEventListener('click', () => {calcDisplay.textContent += calcButton.textContent
-   
+   calcButton.addEventListener('click', () => {
 
+    // if there is already an operator displayed, don't concatenate the next number typed in - 
+    // rather, overwrite the operator on display with the second number.
+    if (operatorCount > 0){
+        operatorCount = 0;
+        calcDisplay.textContent = calcButton.textContent;
+    }
+    else{
+    calcDisplay.textContent += calcButton.textContent;
+    }
+    numbers[arrIndex] += calcButton.textContent;
+   
 });
    buttonArea.appendChild(calcButton);
 }
 
+// attach event listeners to operator buttons.
 
+let buttAdd = document.querySelector('#buttAdd');
+let buttSubtract = document.querySelector('#buttSubtract');
+let ButtMultiply = document.querySelector('#ButtMultiply');
+let ButtDivide = document.querySelector('#ButtDivide');
+let ButtEqual = document.querySelector('#ButtEqual');
+
+// attach event listeners to said operator buttons.
+buttAdd.addEventListener('click', areThereTwoArguments);
+buttSubtract.addEventListener('click', areThereTwoArguments);
+ButtMultiply.addEventListener('click', areThereTwoArguments);
+ButtDivide.addEventListener('click', areThereTwoArguments);
+ButtEqual.addEventListener('click', areThereTwoArguments);
 
